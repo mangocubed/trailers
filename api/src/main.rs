4c51097271f60d9e471fa3d3bf27676141ff2d0f
Javier E. <jaedns@gmail.com>
2026-01-28 21:19:46 -0300

    Add token validation for GraphQL endpoint

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,10 +1,12 @@
 use async_graphql::extensions::apollo_persisted_queries::{
     ApolloPersistedQueries, LruCacheStorage,
 };
-use async_graphql::extensions::{ApolloTracing, Tracing};
-use async_graphql_axum::GraphQL;
-use axum::response::IntoResponse;
-use axum::routing::{get, post_service};
+use async_graphql::extensions::{ApolloTracing, Logger};
+use async_graphql_axum::{GraphQLBatchRequest, GraphQLResponse};
+use axum::extract::State;
+use axum::http::{HeaderMap, HeaderName, StatusCode};
+use axum::response::{ErrorResponse, IntoResponse};
+use axum::routing::{get, post};
 use axum::{Json, Router};
 use tokio::net::TcpListener;
 use tower_http::trace::TraceLayer;
@@ -14,8 +16,53 @@ use trailers_core::Info;
 use trailers_core::config::API_CONFIG;
 use trailers_core::graphql::{GraphqlSchema, GraphqlSchemaExt};
 
+const ERROR_FORBIDDEN: (StatusCode, &str) = (StatusCode::FORBIDDEN, "Forbidden");
+const HEADER_X_TOKEN: HeaderName = HeaderName::from_static("x-token");
+
+trait HttpError<T> {
+    fn or_forbidden(self) -> Result<T, ErrorResponse>;
+}
+
+impl<T> HttpError<T> for Option<T> {
+    fn or_forbidden(self) -> Result<T, ErrorResponse> {
+        match self {
+            Some(value) => Ok(value),
+            None => Err(ERROR_FORBIDDEN.into()),
+        }
+    }
+}
+
+impl<T, E> HttpError<T> for Result<T, E> {
+    fn or_forbidden(self) -> Result<T, ErrorResponse> {
+        match self {
+            Ok(value) => Ok(value),
+            Err(_) => Err(ERROR_FORBIDDEN.into()),
+        }
+    }
+}
+
 async fn get_index() -> impl IntoResponse {
     Json(Info::default())
+}
+
+async fn post_graphql(
+    State(schema): State<GraphqlSchema>,
+    headers: HeaderMap,
+    batch_request: GraphQLBatchRequest,
+) -> Result<GraphQLResponse, ErrorResponse> {
+    let batch_request = batch_request.into_inner();
+
+    let x_token = headers
+        .get(HEADER_X_TOKEN)
+        .or_forbidden()?
+        .to_str()
+        .or_forbidden()?;
+
+    if API_CONFIG.tokens().contains(&x_token) && API_CONFIG.old_tokens().contains(&x_token) {
+        return Err(ERROR_FORBIDDEN.into());
+    }
+
+    Ok(schema.execute_batch(batch_request).await.into())
 }
 
 #[tokio::main]
@@ -32,7 +79,7 @@ async fn main() {
 
     let mut graphql_schema_builder = GraphqlSchema::builder()
         .extension(ApolloPersistedQueries::new(LruCacheStorage::new(1024)))
-        .extension(Tracing);
+        .extension(Logger);
 
     graphql_schema_builder = if !cfg!(debug_assertions) {
         graphql_schema_builder.disable_introspection()
@@ -44,7 +91,8 @@ async fn main() {
 
     let router = Router::new()
         .route("/", get(get_index))
-        .route("/graphql", post_service(GraphQL::new(graphql_schema)))
+        .route("/graphql", post(post_graphql))
+        .with_state(graphql_schema)
         .layer(TraceLayer::new_for_http());
 
     let api_address = &API_CONFIG.address;
--- core/src/config.rs
+++ core/src/config.rs
@@ -1,3 +1,4 @@
+use std::net::SocketAddr;
 use std::sync::LazyLock;
 
 use envconfig::Envconfig;
@@ -7,5 +8,22 @@ pub static API_CONFIG: LazyLock<ApiConfig> = LazyLock::new(|| ApiConfig::init_fr
 #[derive(Envconfig)]
 pub struct ApiConfig {
     #[envconfig(from = "API_ADDRESS", default = "127.0.0.1:8000")]
-    pub address: String,
+    pub address: SocketAddr,
+    #[envconfig(from = "API_OLD_TOKENS", default = "")]
+    old_tokens: String,
+    #[envconfig(from = "API_TOKENS", default = "trailers_app")]
+    tokens: String,
+}
+
+impl ApiConfig {
+    pub fn old_tokens(&self) -> Vec<&str> {
+        self.old_tokens
+            .split(',')
+            .map(|token| token.trim())
+            .collect()
+    }
+
+    pub fn tokens(&self) -> Vec<&str> {
+        self.tokens.split(',').map(|token| token.trim()).collect()
+    }
 }
